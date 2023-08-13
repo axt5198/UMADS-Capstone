c78ffdbe1fc4357db55f2c234bb241acc26eb5e4
Ayrton Trujillo <ayrtonx123@gmail.com>
2023-08-12 20:50:00 -0400

    Update dashboard link

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/shared/MHWJ6NTPM?:display_count=n&:origin=viz_share_link";
+const url = "https://public.tableau.com/views/CapstoneRealEstateMarketDashboard/RealEstateMarketTrends?:language=en-US&:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
